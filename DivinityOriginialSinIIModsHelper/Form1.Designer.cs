7b6416ad00bab71c1cd7fcaaead6c3abb585bff2
Tomas Hansson <tomashansson1984@gmail.com>
2019-08-29 19:13:03 +0200

    First iteration

--- DivinityOriginialSinIIModsHelper/Form1.Designer.cs
+++ DivinityOriginialSinIIModsHelper/Form1.Designer.cs
@@ -28,13 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.steamButton = new System.Windows.Forms.Button();
+            this.documentsButton = new System.Windows.Forms.Button();
+            this.steamTextBox = new System.Windows.Forms.TextBox();
+            this.documentsTextBox = new System.Windows.Forms.TextBox();
+            this.steamLabel = new System.Windows.Forms.Label();
+            this.documentsLabel = new System.Windows.Forms.Label();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.moveLabel = new System.Windows.Forms.Label();
+            this.moveTextBox = new System.Windows.Forms.TextBox();
+            this.steamDefault = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.storeFoldersButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            // 
+            // steamButton
+            // 
+            this.steamButton.Location = new System.Drawing.Point(12, 27);
+            this.steamButton.Name = "steamButton";
+            this.steamButton.Size = new System.Drawing.Size(123, 23);
+            this.steamButton.TabIndex = 0;
+            this.steamButton.Text = "Steam Folder";
+            this.steamButton.UseVisualStyleBackColor = true;
+            this.steamButton.Click += new System.EventHandler(this.SteamButton_Click);
+            // 
+            // documentsButton
+            // 
+            this.documentsButton.Location = new System.Drawing.Point(12, 105);
+            this.documentsButton.Name = "documentsButton";
+            this.documentsButton.Size = new System.Drawing.Size(123, 23);
+            this.documentsButton.TabIndex = 1;
+            this.documentsButton.Text = "Documents Folder";
+            this.documentsButton.UseVisualStyleBackColor = true;
+            this.documentsButton.Click += new System.EventHandler(this.DocumentsButton_Click);
+            // 
+            // steamTextBox
+            // 
+            this.steamTextBox.Enabled = false;
+            this.steamTextBox.Location = new System.Drawing.Point(12, 62);
+            this.steamTextBox.Name = "steamTextBox";
+            this.steamTextBox.Size = new System.Drawing.Size(642, 20);
+            this.steamTextBox.TabIndex = 2;
+            // 
+            // documentsTextBox
+            // 
+            this.documentsTextBox.Enabled = false;
+            this.documentsTextBox.Location = new System.Drawing.Point(12, 134);
+            this.documentsTextBox.Name = "documentsTextBox";
+            this.documentsTextBox.Size = new System.Drawing.Size(642, 20);
+            this.documentsTextBox.TabIndex = 3;
+            // 
+            // steamLabel
+            // 
+            this.steamLabel.AutoSize = true;
+            this.steamLabel.Location = new System.Drawing.Point(12, 9);
+            this.steamLabel.Name = "steamLabel";
+            this.steamLabel.Size = new System.Drawing.Size(102, 13);
+            this.steamLabel.TabIndex = 4;
+            this.steamLabel.Text = "Select Steam Folder";
+            // 
+            // documentsLabel
+            // 
+            this.documentsLabel.AutoSize = true;
+            this.documentsLabel.Location = new System.Drawing.Point(9, 89);
+            this.documentsLabel.Name = "documentsLabel";
+            this.documentsLabel.Size = new System.Drawing.Size(126, 13);
+            this.documentsLabel.TabIndex = 5;
+            this.documentsLabel.Text = "Select Documents Folder";
+            // 
+            // moveButton
+            // 
+            this.moveButton.Location = new System.Drawing.Point(12, 182);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(123, 23);
+            this.moveButton.TabIndex = 6;
+            this.moveButton.Text = "Move";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.MoveButton_Click);
+            // 
+            // moveLabel
+            // 
+            this.moveLabel.AutoSize = true;
+            this.moveLabel.Location = new System.Drawing.Point(12, 166);
+            this.moveLabel.Name = "moveLabel";
+            this.moveLabel.Size = new System.Drawing.Size(63, 13);
+            this.moveLabel.TabIndex = 7;
+            this.moveLabel.Text = "Move Mods";
+            // 
+            // moveTextBox
+            // 
+            this.moveTextBox.Enabled = false;
+            this.moveTextBox.Location = new System.Drawing.Point(12, 211);
+            this.moveTextBox.Name = "moveTextBox";
+            this.moveTextBox.Size = new System.Drawing.Size(642, 20);
+            this.moveTextBox.TabIndex = 8;
+            // 
+            // steamDefault
+            // 
+            this.steamDefault.AutoSize = true;
+            this.steamDefault.Location = new System.Drawing.Point(141, 9);
+            this.steamDefault.Name = "steamDefault";
+            this.steamDefault.Size = new System.Drawing.Size(216, 13);
+            this.steamDefault.TabIndex = 9;
+            this.steamDefault.Text = "The default is C:\\Program Files (x86)\\Steam.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(505, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Ensure this is where Divinity is installed (it\'s possible to have different insta" +
+    "llation-paths for different games).";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "This should contain a steamapp-folder.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(401, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Beware - older versions of duplicate mods in your mods-folder, if any, will be de" +
+    "leted.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(337, 26);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "The default is C:\\Users\\<your username>\\Documents.\r\nThis should be prefilled by t" +
+    "he program, if not, select the correct folder.";
+            // 
+            // storeFoldersButton
+            // 
+            this.storeFoldersButton.Location = new System.Drawing.Point(499, 4);
+            this.storeFoldersButton.Name = "storeFoldersButton";
+            this.storeFoldersButton.Size = new System.Drawing.Size(155, 23);
+            this.storeFoldersButton.TabIndex = 14;
+            this.storeFoldersButton.Text = "Store Folder Locations";
+            this.storeFoldersButton.UseVisualStyleBackColor = true;
+            this.storeFoldersButton.Click += new System.EventHandler(this.StoreFoldersButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(666, 246);
+            this.Controls.Add(this.storeFoldersButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.steamDefault);
+            this.Controls.Add(this.moveTextBox);
+            this.Controls.Add(this.moveLabel);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.documentsLabel);
+            this.Controls.Add(this.steamLabel);
+            this.Controls.Add(this.documentsTextBox);
+            this.Controls.Add(this.steamTextBox);
+            this.Controls.Add(this.documentsButton);
+            this.Controls.Add(this.steamButton);
+            this.Name = "Form1";
+            this.Text = "Divinity Original Sin II - Mods Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button steamButton;
+        private System.Windows.Forms.Button documentsButton;
+        private System.Windows.Forms.TextBox steamTextBox;
+        private System.Windows.Forms.TextBox documentsTextBox;
+        private System.Windows.Forms.Label steamLabel;
+        private System.Windows.Forms.Label documentsLabel;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Label moveLabel;
+        private System.Windows.Forms.TextBox moveTextBox;
+        private System.Windows.Forms.Label steamDefault;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button storeFoldersButton;
     }
 }
 
--- DivinityOriginialSinIIModsHelper/Form1.cs
+++ DivinityOriginialSinIIModsHelper/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,115 @@ namespace DivinityOriginialSinIIModsHelper
         public Form1()
         {
             InitializeComponent();
+            string configurationsFilePath = GetConfigurationsFilePath();
+            if (File.Exists(configurationsFilePath))
+            {
+                using (StreamReader streamReader = new StreamReader(configurationsFilePath))
+                {
+                    string configuration = streamReader.ReadLine();
+                    steamTextBox.Text = configuration.Split('|')[0];
+                    documentsTextBox.Text = configuration.Split('|')[1];
+                }
+            }
+            else
+            {
+                steamTextBox.Text = @"C:\Program Files (x86)\Steam";
+                documentsTextBox.Text = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents");
+            }
+        }
+
+        private void SteamButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                steamTextBox.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void DocumentsButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                documentsTextBox.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void MoveButton_Click(object sender, EventArgs e)
+        {
+            int numberOfNewMods = 0, numberOfUpdatedMods = 0, numberOfUnchangedMods = 0;
+
+            string modsFolder = string.Concat(documentsTextBox.Text, @"\Larian Studios\Divinity Original Sin 2 Definitive Edition\Mods");
+            if (!Directory.Exists(modsFolder))
+            {
+                MessageBox.Show("Cannot find the mods-folder in the selected Documents-folder." +
+                    "\n\nEnsure that the game is installed correctly and that you've selected your Documents folder.");
+                moveTextBox.Text = "Failed operation.";
+                return;
+            }
+            string[] currentModsFilePaths = Directory.GetFiles(modsFolder);
+            List<string> currentModsFileNames = new List<string>();
+            foreach (var modFilePath in currentModsFilePaths)
+                currentModsFileNames.Add(Path.GetFileName(modFilePath));
+
+            string workshopFolder = string.Concat(steamTextBox.Text, @"\steamapps\workshop\content\435150");
+            if (!Directory.Exists(workshopFolder))
+            {
+                MessageBox.Show("Cannot find the workshop-folder in the selected Steam-folder."
+                    + "\n\nEnsure that the game is installed correctly and that you've selected the correct Steam folder.");
+                moveTextBox.Text = "Failed operation.";
+                return;
+            }
+
+            moveTextBox.Text = "Update in progress...";
+            string[] subfolders = Directory.GetDirectories(workshopFolder);
+            foreach (string folder in subfolders)
+            {
+                string modFilePath = Directory.GetFiles(folder)[0];
+                string modFileName = Path.GetFileName(modFilePath);
+                if (currentModsFileNames.Contains(modFileName))
+                {
+                    // File with same name already exists.
+                    string currentFilePath = string.Concat(modsFolder, @"\" + modFileName);
+                    FileInfo newFile = new FileInfo(modFilePath);
+                    FileInfo currentFile = new FileInfo(currentFilePath);
+                    if (newFile.Length != currentFile.Length ||
+                        newFile.LastWriteTime > currentFile.LastWriteTime)
+                    {
+                        // Files are different -> Replace with new file.
+                        File.Delete(currentFilePath);
+                        File.Copy(modFilePath, currentFilePath);
+                        numberOfUpdatedMods++;
+                    }
+                    else
+                    {
+                        // Files are equal -> Keep the current file.
+                        numberOfUnchangedMods++;
+                    }
+                }
+                else
+                {
+                    // New mod to add.
+                    string desinationFilePath = string.Concat(modsFolder, @"\" + modFileName);
+                    File.Copy(modFilePath, desinationFilePath);
+                    numberOfNewMods++;
+                }
+            }
+
+            if (numberOfNewMods == 0 && numberOfUpdatedMods == 0)
+                moveTextBox.Text = $"No new or updated mods were found. Found {numberOfUnchangedMods} previously installed mods.";
+            else
+                moveTextBox.Text = $"New mods: {numberOfNewMods}. Updated mods: {numberOfUpdatedMods}. Unchanged mods: {numberOfUnchangedMods}." +
+                    $"Total: {numberOfNewMods + numberOfUpdatedMods + numberOfUnchangedMods}.";
+        }
+
+        private string GetConfigurationsFilePath()
+        {
+            string directory = Directory.GetCurrentDirectory();
+            string configurationsfile = "dosiimh-configuration.txt";
+            return string.Concat(directory, @"\" + configurationsfile);
+        }
+
+        private void StoreFoldersButton_Click(object sender, EventArgs e)
+        {
+            string configurationsFilePath = GetConfigurationsFilePath();
+            using (StreamWriter streamWriter = new StreamWriter(configurationsFilePath))
+                streamWriter.WriteLine(steamTextBox.Text + "|" + documentsTextBox.Text);
         }
     }
 }
